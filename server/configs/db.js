07d588835483d689a2894aabdcf3884d002daacc
SanketSaha04 <sanketsaha04@gmail.com>
2026-01-11 19:12:14 +0530

    Solved db connection name bug

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=>{
     try{
+      // console.log("Connected DB:", mongoose.connection.name)
+
       mongoose.connection.on('connected',()=> console.log("Database Connected")
     );
     await mongoose.connect(`${process.env.MONGODB_URI}/greencart`)
